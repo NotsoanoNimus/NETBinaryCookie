0f00ae872da40f07172fcd16431bda3608249a7b
NotsoanoNimus <zack.puhl@galacticadvisors.com>
2023-02-18 03:34:30 -0500

    Add Endianness attribute to indicate for data-type

---
+++ NETBinaryCookie/NETBinaryCookie/EndiannessAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace NETBinaryCookie;
+
+internal sealed class EndiannessAttribute : Attribute 
+{
+    internal enum Endianness
+    {
+        LittleEndian,
+        BigEndian
+    }
+    
+    internal Endianness Endian { get; }
+
+    internal EndiannessAttribute(Endianness endianness)
+    {
+        this.Endian = endianness;
+    }
+}
