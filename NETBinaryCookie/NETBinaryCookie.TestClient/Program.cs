8d3634f289711259ebb06364136c21d2a24c1fab
NotsoanoNimus <zack.puhl@galacticadvisors.com>
2023-02-18 03:29:10 -0500

    Add test client

---
+++ NETBinaryCookie/NETBinaryCookie.TestClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using NETBinaryCookie;
+
+
+const string fileName = @"C:\Users\ZackPuhl\Downloads\com.microsoft.teams.binarycookies";
+
+var binaryCookieJar = NetBinaryCookie.ReadFromFile(fileName);
+
+foreach (var cookie in binaryCookieJar.GetCookies())
+{
+    Console.WriteLine($"DOMAIN: {cookie.Domain}\nPATH: {cookie.Path}\nVALUE: {cookie.Value}\n");
+}
+
+Environment.Exit(0);
