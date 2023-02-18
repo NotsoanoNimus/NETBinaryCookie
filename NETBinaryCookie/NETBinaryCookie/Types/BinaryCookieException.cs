126e4c8d159adffc60359216755414debd389739
NotsoanoNimus <zack.puhl@galacticadvisors.com>
2023-02-18 03:32:02 -0500

    Add BinaryCookieException custom exception type

---
+++ NETBinaryCookie/NETBinaryCookie/Types/BinaryCookieException.cs
@@ -0,0 +1,17 @@
+﻿namespace NETBinaryCookie.Types;
+
+public class BinaryCookieException : Exception
+{
+    public BinaryCookieException()
+    {
+    }
+
+    public BinaryCookieException(string message) : base(message)
+    {
+    }
+
+    public BinaryCookieException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
