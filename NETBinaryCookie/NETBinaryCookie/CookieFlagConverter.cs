3cc085cae3d83372d8839360f3cc4ae3f79d9725
NotsoanoNimus <zack.puhl@galacticadvisors.com>
2023-02-19 22:09:53 -0500

    Add CookieFlagConverter for better JSON parsing

---
+++ NETBinaryCookie/NETBinaryCookie/CookieFlagConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NETBinaryCookie;
+
+public sealed class CookieFlagConverter : JsonConverter<NetBinaryCookie.CookieFlag[]>
+{
+    public override NetBinaryCookie.CookieFlag[] Read(ref Utf8JsonReader reader, Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        var result = new List<NetBinaryCookie.CookieFlag>();
+        
+        while (reader.Read())
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    result.Add((NetBinaryCookie.CookieFlag)Enum.Parse(typeof(NetBinaryCookie.CookieFlag),
+                        reader.GetString()!));
+                    break;
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    public override void Write(Utf8JsonWriter writer, NetBinaryCookie.CookieFlag[] value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, value.Select(x => x.ToString()), options);
+    }
+}
--- NETBinaryCookie/NETBinaryCookie/Types/BinaryCookie.cs
+++ NETBinaryCookie/NETBinaryCookie/Types/BinaryCookie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace NETBinaryCookie.Types;
 
@@ -10,6 +11,13 @@ public class BinaryCookie
         string.Equals(left.Path, right.Path, StringComparison.CurrentCultureIgnoreCase);
     
     public static bool operator !=(BinaryCookie left, BinaryCookie right) => !(left == right);
+
+    public override string ToString()
+    {
+        return $"DOMAIN: {this.Domain}\nNAME: {this.Name}\nPATH: {this.Path}\n" +
+               $"FLAGS: {(this.Flags.Length > 0 ? string.Join(", ", this.Flags) : "(none)")}\n" +
+               $"SET: {this.Creation}\nEXPIRES: {this.Expiration}\nVALUE: {this.Value}\n";
+    }
 
     [Required] public DateTime Expiration { get; set; }
     
@@ -25,5 +33,6 @@ public class BinaryCookie
     
     [Required] public string Value { get; set; } = string.Empty;
     
-    [Required] public NetBinaryCookie.CookieFlag[] Flags { get; set; } = Array.Empty<NetBinaryCookie.CookieFlag>();
+    [Required] [JsonConverter(typeof(CookieFlagConverter))]
+    public NetBinaryCookie.CookieFlag[] Flags { get; set; } = Array.Empty<NetBinaryCookie.CookieFlag>();
 }
